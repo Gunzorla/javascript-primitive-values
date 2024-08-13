e96f1b2276b6325bcaea685145010cf27f06c03d
Gunzorla <g.gun.n.1991@gmail.com>
2024-08-14 01:23:16 +0700

    add ex 1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalProductPrice = 27000
+let productDiscount = totalProductPrice * 25 / 100
+let afterDiscount = totalProductPrice - productDiscount
+
+console.log(afterDiscount)
